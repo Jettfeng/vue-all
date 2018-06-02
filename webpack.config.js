fc3115c394e66083341960bec1f66498b371011c
jettFeng <1130034920@>
2018-06-02 14:53:21 +0800

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path')
-module.exports = {
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const isDev = process.env.NODE_ENV == 'development'//判断是否是开发环境
+
+const config = {
+    target:'web',
     entry:path.join(__dirname,'src/index.js'),
     output:{
         filename:'bundle.js',
@@ -39,5 +45,31 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins:[
+        new webpack.DefinePlugin({//
+            'process.env':{
+                NODE_ENV:isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
 }
+
+if(isDev){
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port:8000,//启动端口
+        host:'192.168.0.107',//
+        overlay:{//将错误显示在网页上
+            errors:true
+        },
+        open:true,//自动打开页面
+        hot:true//单页应用修改数据时只重新渲染当组件
+    }
+    config.plugins.push(//不刷新页面更改数据配置
+        new webpack.HotModuleReplacementPlugin(),//启动webpackhotmodule功能
+        new webpack.NoEmitOnErrorsPlugin()//
+    )
+}
+module.exports = config
