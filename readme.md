9f7be457c6ba11184a50a06e9882f5f1a1fd55d9
jettFeng <1130034920@>
2018-06-02 13:21:24 +0800

    commit

--- readme.md
+++ readme.md
@@ -1,5 +1,1 @@
-<h3>todo2-1:webpack项目配置<h3>
-<p>安装vue：npm install vue </p>
-<p>安装vue-loader：npm install vue-loader</p>
-<p>安装vue-template-compiler：npm install vue-template-compiler</p>
-<p>安装webpack：npm install webpack </p>
+<h3>todo2-2:webpack配置项目家长各种静态资源及css预处理器<h3>
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,33 @@ module.exports = {
             {
                 test:/\.vue$/,
                 use:'vue-loader'
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.styl$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test:/\.(gif|jpg|jpeg|png|svg)$/,
+                use:[
+                    {
+                        loader: 'url-loader',
+                        options: {
+                          limit: 8192,//小于8192kb转换成base64
+                          name:'[name].[ext]'//ext代表后缀扩展名
+                        }
+                      }
+                ]
             }
         ]
     }
